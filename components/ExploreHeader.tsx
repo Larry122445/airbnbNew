21ebe52c4ec4418d5b17c0a14c56c3b1f2b2e2f9
PapaK20 <nasarepeekay@gmail.com>
2024-06-09 01:21:48 +0000

    First Commit

---
+++ components/ExploreHeader.tsx
@@ -0,0 +1,190 @@
+import { View, Text, SafeAreaView, StyleSheet, TouchableOpacity, ScrollView } from 'react-native';
+import { GestureHandlerRootView } from 'react-native-gesture-handler';
+import { NativeViewGestureHandler } from 'react-native-gesture-handler';
+import { useRef, useState } from 'react';
+import Colors from '@/constants/Colors';
+import { Ionicons } from '@expo/vector-icons';
+import { MaterialIcons } from '@expo/vector-icons';
+import * as Haptics from 'expo-haptics';
+import { Link } from 'expo-router';
+
+const categories = [
+  {
+    name: 'Tiny homes',
+    icon: 'home',
+  },
+  {
+    name: 'Cabins',
+    icon: 'house-siding',
+  },
+  {
+    name: 'Trending',
+    icon: 'local-fire-department',
+  },
+  {
+    name: 'Play',
+    icon: 'videogame-asset',
+  },
+  {
+    name: 'City',
+    icon: 'apartment',
+  },
+  {
+    name: 'Beachfront',
+    icon: 'beach-access',
+  },
+  {
+    name: 'Countryside',
+    icon: 'nature-people',
+  },
+];
+
+interface Props {
+  onCategoryChanged: (category: string) => void;
+}
+
+const ExploreHeader = ({ onCategoryChanged }: Props) => {
+  const scrollRef = useRef<ScrollView>(null);
+  const itemsRef = useRef<Array<TouchableOpacity | null>>([]);
+  const [activeIndex, setActiveIndex] = useState(1);
+
+  const selectCategory = (index: number) => {
+    const selected = itemsRef.current[index];
+    setActiveIndex(index);
+    selected?.measure((x) => {
+      scrollRef.current?.scrollTo({ x: x - 16, y: 5, animated: true });
+    });
+    Haptics.impactAsync(Haptics.ImpactFeedbackStyle.Light);
+    onCategoryChanged(categories[index].name);
+  };
+
+  return (
+      <View style={styles.container}>
+        <View style={styles.actionRow}>
+          <Link href={'/(modals)/booking'} asChild>
+            <TouchableOpacity>
+              <View style={styles.searchBtn}>
+                <Ionicons name="search" size={24} />
+                <View>
+                  <Text style={{ fontFamily: 'mon-sb' }}>Where to?</Text>
+                  <Text style={{ color: Colors.grey, fontFamily: 'mon' }}>Anywhere · Any week</Text>
+                </View>
+              </View>
+            </TouchableOpacity>
+          </Link>
+          <TouchableOpacity style={styles.filterBtn}>
+            <Ionicons name="options-outline" size={24} />
+          </TouchableOpacity>
+        </View>
+
+        <GestureHandlerRootView style={{ flex: 1 }}>
+        <ScrollView
+          ref={scrollRef}
+          horizontal
+          showsHorizontalScrollIndicator={false}
+          contentContainerStyle={{
+            alignItems: 'center',
+            gap: 33,
+            paddingHorizontal: 10,
+          }}>
+          {categories.map((item, index) => (
+            <TouchableOpacity
+              onPress={() => selectCategory(index)}
+              key={index}
+              ref={(el) => itemsRef.current[index] = el}
+              style={activeIndex === index ? styles.categoriesBtnActive : styles.categoriesBtn}
+              >
+              <MaterialIcons
+                name={item.icon as any}
+                size={24}
+                color={activeIndex === index ? '#000' : Colors.grey}
+              />
+              <Text style={activeIndex === index ? styles.categoryTextActive : styles.categoryText} >
+                {item.name}
+              </Text>
+            </TouchableOpacity>
+          ))}
+        </ScrollView>
+        </GestureHandlerRootView>
+      </View>
+    
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: '#fff',
+    paddingTop: 8,
+    paddingBottom: 5,
+    height: 143,
+    elevation: 8,
+    shadowColor: '#000',
+    shadowOpacity: 0.1,
+    shadowRadius: 8,
+    shadowOffset: {
+      width: 1,
+      height: 15,
+    },
+  },
+  actionRow: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+    paddingHorizontal: 20,
+    paddingBottom: 14,
+  },
+
+  searchBtn: {
+    backgroundColor: '#fff',
+    flexDirection: 'row',
+    gap: 8,
+    padding: 14,
+    alignItems: 'center',
+    width: 280,
+    borderWidth: StyleSheet.hairlineWidth,
+    borderColor: '#c2c2c2',
+    borderRadius: 30,
+    elevation: 2,
+    shadowColor: '#000',
+    shadowOpacity: 0.12,
+    shadowRadius: 8,
+    shadowOffset: {
+      width: 1,
+      height: 5,
+    },
+  },
+  filterBtn: {
+    padding: 13,
+    borderWidth: 1,
+    borderColor: '#A2A0A2',
+    borderRadius: 24,
+  },
+  categoryText: {
+    fontSize: 13,
+    textAlign: 'center',
+    fontFamily: 'mon-sb',
+    color: Colors.grey,
+  },
+  categoryTextActive: {
+    fontSize: 17,
+    textAlign: 'center',
+    fontFamily: 'mon-sb',
+    color: '#000',
+  },
+  categoriesBtn: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+    paddingBottom: 5,
+  },
+  categoriesBtnActive: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+    borderBottomColor: '#000',
+    borderBottomWidth: 2,
+    paddingBottom: 8,
+  },
+});
+
+export default ExploreHeader;
